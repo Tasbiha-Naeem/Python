98f4e6370ce8e8c1a93347aeaf105b6d1c806809
Tasbiha Naeem <tasbihanaeem123@gmail.com>
2025-03-19 21:33:51 +0500

    Create double and single quote escape sequence in python.py

---
+++ double and single quote escape sequence in python.py
@@ -0,0 +1,8 @@
+# \" is used to include a double quote inside a double-quoted string
+print("This is a \"double quote\" inside a string.")  
+# Output: This is a "double quote" inside a string.
+
+# \' is used for a single quote inside a single-quoted string
+print('It\'s a great day!')  
+# Output: It's a great day!
+
