2bbb32d055a2c066a1ee93385d39ff36d1bc1775
Tasbiha Naeem <tasbihanaeem123@gmail.com>
2025-03-19 22:06:31 +0500

    Create end in python.py

---
+++ end in python.py
@@ -0,0 +1,3 @@
+#In Python, the end parameter in the print() function defines what gets printed at the end of the output, instead of the default new line (\n).
+print("Hello", end="")
+print("World")
