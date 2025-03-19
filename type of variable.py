e8bcdc2fdbfec4b91548eb6fece4f646e1b02a20
Tasbiha Naeem <tasbihanaeem123@gmail.com>
2025-03-20 01:10:48 +0500

    Create type of variable.py

---
+++ type of variable.py
@@ -0,0 +1,7 @@
+#In Python, everything inside quotes (' ' or " ") is treated as a string (str), even if it's a single character
+a="happy"
+b=7
+c='t'
+print("The type of a is ",type(a))
+print("The type of b is ",type(b))
+print("The type of c is ",type(c))
