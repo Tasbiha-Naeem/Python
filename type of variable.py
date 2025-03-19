491e9ec76369fcd3ba50803a5d7738d3900f235b
Tasbiha Naeem <tasbihanaeem123@gmail.com>
2025-03-20 03:02:03 +0500

    Update type of variable.py

--- type of variable.py
+++ type of variable.py
@@ -3,7 +3,11 @@ a="happy"
 b=7
 c='t'
 d=True
+e=3.14
+f=complex(8,9)
 print("The type of a is ",type(a))
 print("The type of b is ",type(b))
 print("The type of c is ",type(c))
 print("The type of d is ",type(d))
+print("The type of e is ",type(e))
+print("The type of f is ",type(f))
