89b1482587fdfb4cc4185e71a3895b89aabcd316
Tasbiha Naeem <tasbihanaeem123@gmail.com>
2025-03-20 05:37:10 +0500

    Create calculator in python.py

---
+++ calculator in python.py
@@ -0,0 +1,7 @@
+#calculator
+a=50
+b=20
+print("The sum of",a,"+",b,"is",a+b)
+print("The difference of",a,"-",b,"is",a-b)
+print("The product of",a,"*",b,"is",a*b)
+print("The division of",a,"/",b,"is",a/b)
