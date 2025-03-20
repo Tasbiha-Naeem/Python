15fe7d0e39ac55d3f8400f401fe1d859fe5e3fc6
Tasbiha Naeem <tasbihanaeem123@gmail.com>
2025-03-20 05:32:50 +0500

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,10 @@
+#operators
+a=100;
+b=20;
+print(a+b)   #addition
+print(a-b)   #subtration
+print(a*b)   #multiplication
+print(a/b)   #division
+print(a%b)   #modulus
+print(a//b)  #floor division rounds the result down to the nearest whole number.
+print(a**b)  #exponential
