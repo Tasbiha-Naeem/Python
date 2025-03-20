4b86a854e030a6027e9fafff278caceb9b071990
Tasbiha Naeem <tasbihanaeem123@gmail.com>
2025-03-20 06:02:35 +0500

    Create Taking user input in python.py

---
+++ Taking user input in python.py
@@ -0,0 +1,6 @@
+name=input("Enter your name:")
+print("My name is",name)
+num1=input("Enter the first number:")
+num2=input("Enter the second number:")
+#you have to convert the variable in int by the help of tye casting because when a user input the number the compiler consider it as a string.
+print(int(num1)+int(num2))
