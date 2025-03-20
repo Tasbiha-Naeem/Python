16d7d2015501a75f9457f3ddaf562caea8bfc933
Tasbiha Naeem <tasbihanaeem123@gmail.com>
2025-03-20 05:46:47 +0500

    Update typecasting in python.py

--- typecasting in python.py
+++ typecasting in python.py
@@ -1,3 +1,4 @@
+#There are two types of type casting one is explicit typecasting which is done by the programmer or a developer and other one is implicit typecasting which is done by interpreter.
 a="6"
 b="5"
 print(a+b)
