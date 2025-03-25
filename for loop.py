381d6768b88dac628e3d30ad401c4d0de5e8adaf
Tasbiha Naeem <tasbihanaeem123@gmail.com>
2025-03-25 13:53:56 +0500

    Create for loop.py

---
+++ for loop.py
@@ -0,0 +1,14 @@
+name="Tasbiha"
+for i in name:
+    print(i,end="\n")
+colors=["red","green","blue","yellow"]
+for color in colors:
+    print(color)
+    for i in color:
+        print(i) 
+for j in range(1,11):   
+    print(j)
+#range(start,stop,step)
+for t in range(1,11,2):
+    print(t)
+# In loops, the step defines how much the counter variable increases or decreases in each iteration.
