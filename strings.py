585a4ce8c67ab48557471547debf81524f889dfd
Tasbiha Naeem <tasbihanaeem123@gmail.com>
2025-03-28 19:35:14 +0500

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,27 @@
+name="Tasbiha"
+print("Hello,"+name)
+intro="Hey I am Tasbiha \"I am interested in tech\""
+print(intro)
+
+#multiline string
+multi_line='''HEY
+I AM TASBIHA
+I AM EAGER TO LEARN TECH'''
+print(multi_line)
+
+GREETING="Good morning"
+print(GREETING)
+
+#In python string is like an array of character
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
+#looping through the string
+for character in name:
+    print(character)
+for index in multi_line:
+    print(index)
