7bdc78c2975a28bd5ef2df9108e1314d6d8b5781
Tasbiha Naeem <tasbihanaeem123@gmail.com>
2025-03-28 19:57:58 +0500

    Create string slicing and operations on strings.py

---
+++ string slicing and operations on strings.py
@@ -0,0 +1,11 @@
+name="harry,code"  
+print(name[0:5]) #including 0 but not 4.
+#length of a string
+print(len(name))
+fruit="Strawberry"
+len1=len(fruit)
+print("Strawberry is a",len1,"character word.")
+dryfruit="almond"
+print(dryfruit[0:-3])#interpreter take it as (dryfruit[0:len(dyrfruit)-3])
+
+
